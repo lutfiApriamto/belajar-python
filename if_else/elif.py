7106642d575beacb3a4d36dbd09255d3239d3113
LutfiApriamto <afriantolutfi@gmail.com>
2024-11-21 08:51:41 +0700

    menambahkan latihan percabangan

---
+++ if_else/elif.py
@@ -0,0 +1,10 @@
+# Elif statement
+
+nama = input("Siapa nama anda? ")
+
+if nama == "lutfi" :
+    print(f"halo {nama} ganteng")
+elif nama == "keisha" :
+    print(f'halo {nama} cantik')
+else :
+    print(f"hei {nama} kita gak kenal!")
